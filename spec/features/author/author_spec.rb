fa0662d7f39dc78ffe2195be5f15e7bce211abfd
Juliane <julianekleinknecht@gmail.com>
2017-10-29 00:48:38 +0200

    Failing test for #8

--- spec/features/author/author_spec.rb
+++ spec/features/author/author_spec.rb
@@ -5,4 +5,10 @@ describe "Index author page", type: :feature do
 	it "should render without error" do
 		visit "authors"
 	end
+
+	it "should show the first and last name in one colum" do
+		visit "authors"
+
+		find("th", text: "Name")
+	end
 end
