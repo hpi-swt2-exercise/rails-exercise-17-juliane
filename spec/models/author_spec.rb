c43e2858693d168fc36aadc4574328d4d8a38ea1
Juliane <julianekleinknecht@gmail.com>
2017-10-28 21:27:11 +0200

    Failing test for #5

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,11 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe Author, type: :model do |author|
+RSpec.describe "Author", type: :model do |author|
 
-  it "should be created with first name, last name and homepage" do
-		author = Author.new(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/Alan_Turing")
+	author = Author.new(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/Alan_Turing")
+
+	it "should be created with first name, last name and homepage" do
 		expect(author.first_name).to eq("Alan")
 		expect(author.last_name).to eq("Turing")
 		expect(author.homepage).to eq("http://wikipedia.org/Alan_Turing")
 	end
+
+	it "should be able to return the full name" do
+		expect(author.name).to eq("Alan Turing")
+	end
+
 end
