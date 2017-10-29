8f248c22aaa68786ace16c433fc767ab9d352675
Juliane <julianekleinknecht@gmail.com>
2017-10-29 15:01:32 +0100

    Added link to add author at authors index page. Closes #9

--- spec/features/author/author_spec.rb
+++ spec/features/author/author_spec.rb
@@ -3,18 +3,18 @@ require 'rails_helper'
 describe "Index author page", type: :feature do
 
 	it "should render without error" do
-		visit "authors"
+		visit authors_path
 	end
 
 	it "should show the first and last name in one colum" do
-		visit "authors"
+		visit authors_path
 
 		find("th", text: "Name")
 	end
 
 	it "should have a linkt to add author" do
-		visit "authors"
+		visit authors_path
 
-		expect(page).to have_css('a', text: "add author")
+		expect(page).to have_css('a', text: "Add author")
 	end
 end
