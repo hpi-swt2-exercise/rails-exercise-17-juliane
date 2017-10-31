8e297824807b5d08fa788632498116284fdb0162
Juliane <julianekleinknecht@gmail.com>
2017-10-31 15:22:12 +0100

    Fixed test for #19

--- spec/features/author/author_spec.rb
+++ spec/features/author/author_spec.rb
@@ -42,7 +42,7 @@ describe "Index author page", type: :feature do
 	it "should have a link to delete author and delete it when its clicked" do
 		author = FactoryGirl.create :author
 		visit authors_path
-		click_link('Destroy', match: :first)
-		expect(author).to be_nil
+		find("a[href='#{author_path(author)}']", :text => 'Destroy').click
+		expect(Author.where(id: author.id)).not_to exist
 	end
 end
