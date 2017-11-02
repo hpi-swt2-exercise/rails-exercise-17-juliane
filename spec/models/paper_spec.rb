d2e298ade3adcf2fc7b4f7cafebb0fddd49947c4
Juliane <julianekleinknecht@gmail.com>
2017-11-02 15:04:33 +0100

    Failing test for #21

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	it "should be created with title venue and year" do
+		paper = Paper.new(title: "COMPUTING MACHINERY AND INTELLIGENCE", venue: "Mind 49: 433-460", year: 1950)
+		expect(paper.year).to eq(1950)
+		expect(paper.venue).to eq("Mind 49: 433-460")
+		expect(paper.title).to eq("COMPUTING MACHINERY AND INTELLIGENCE")
+	end
 end
