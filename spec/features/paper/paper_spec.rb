c667458d9b0b6b81e7c93a4261e7b62b8de6a6be
Juliane <julianekleinknecht@gmail.com>
2017-11-02 20:54:39 +0100

    Fixed failing test for #27

--- spec/features/paper/paper_spec.rb
+++ spec/features/paper/paper_spec.rb
@@ -7,10 +7,10 @@ describe "Index paper page", type: :feature do
 	end
 
 	it "should show all papers" do
-		visit papers_path
-		FactoryGirl.create :paper
-		expect(page).to have_text('COMPUTING MACHINERY AND INTELLIGENCE')
-    expect(page).to have_text('Mind 49: 433-460')
-    expect(page).to have_text(1950)
+		paper = FactoryGirl.create :paper
+    visit papers_path
+		expect(page).to have_text(paper.title)
+    expect(page).to have_text(paper.venue)
+    expect(page).to have_text(paper.year)
 	end
 end
