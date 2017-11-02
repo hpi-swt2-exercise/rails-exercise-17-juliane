38912e522fa7a7d0808de985458b2f3dd576e0c9
Juliane <julianekleinknecht@gmail.com>
2017-11-02 20:59:22 +0100

    Failing test for #28

--- spec/features/paper/paper_spec.rb
+++ spec/features/paper/paper_spec.rb
@@ -12,5 +12,12 @@ describe "Index paper page", type: :feature do
 		expect(page).to have_text(paper.title)
     expect(page).to have_text(paper.venue)
     expect(page).to have_text(paper.year)
+  end
+
+	it "should have a link to show the authors" do
+		FactoryGirl.create :paper
+		visit papers_path
+
+		expect(page).to have_link('Show')
 	end
 end
