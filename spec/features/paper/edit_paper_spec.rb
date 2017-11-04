5ab85a60c5fba5de10c7e6297636225d6e894c6d
Juliane <julianekleinknecht@gmail.com>
2017-11-04 14:37:34 +0100

    Failing test for #35

--- spec/features/paper/edit_paper_spec.rb
+++ spec/features/paper/edit_paper_spec.rb
@@ -12,5 +12,13 @@ describe "Edit paper page", type: :feature do
 		visit edit_paper_path(paper.id)
 		fill_in "paper_title", :with => "New Title"
 		find('input[type="submit"]').click
-	end
+  end
+
+  it 'should have 5 dropdowns for author' do
+    paper = FactoryGirl.create :paper
+    visit edit_paper_path(paper.id)
+    5.times do |i|
+    	expect(page).to have_field("Author #{i+1}")
+    end
+  end
 end
